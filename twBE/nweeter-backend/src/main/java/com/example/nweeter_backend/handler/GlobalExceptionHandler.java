0b51baa5dc78ba3b55c59cda35c9420a4a2f08ca
leesugi66 <leesulgi66@naver.com>
2025-06-29 18:11:43 +0900

    GlobalExceptionHandler - handleMaxUploadSizeExceededException add

--- twBE/nweeter-backend/src/main/java/com/example/nweeter_backend/handler/GlobalExceptionHandler.java
+++ twBE/nweeter-backend/src/main/java/com/example/nweeter_backend/handler/GlobalExceptionHandler.java
@@ -6,10 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        return new ResponseEntity<>("MaxUploadSizeExceededException", HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(value=Exception.class)
     public ResponseEntity<String> handlerArgumentException(Exception e) {
