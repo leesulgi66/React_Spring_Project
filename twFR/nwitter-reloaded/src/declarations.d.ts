35b6181ff1c5751011c3dc0f634d06dfc733abba
leesugi66 <leesulgi66@naver.com>
2025-07-05 21:11:05 +0900

    test

--- twFR/nwitter-reloaded/src/declarations.d.ts
+++ twFR/nwitter-reloaded/src/declarations.d.ts
@@ -4,7 +4,7 @@
 declare module 'quill-image-resize-module-react';
 
 // store 모듈 타입 선언
-declare module './store' {
+declare module '/store' {
   const store: any; // 실제 store의 타입에 맞게 수정하면 더 좋습니다.
-  export default store;
+  export default store;git
 }
